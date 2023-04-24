40a503bdd4fc1a1b22e52fa2c7c63fb9bb6793ed
Konrad Szroter <73541460+Konkox@users.noreply.github.com>
2023-04-24 08:51:36 +0200

    Add files via upload

---
+++ Api.jsx
@@ -0,0 +1,72 @@
+import Media from  'react-media'
+import {Fragment} from 'react'
+
+
+
+const { useEffect, useState } = require("react");
+
+const Api = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://random-data-api.com/api/v2/banks?size=10")
+      .then((data) => data.json())
+      .then((data_list) => setData(data_list));
+  }, []);
+
+  return (
+    <div>
+      <h2>Najbogatsze banki świata</h2>
+        <ul>
+          {data.map((bankInfo) => (
+            <li key={bankInfo.id}>{bankInfo.bank_name}</li> 
+            
+
+          ))}
+        </ul>
+
+{/* <Media queries={{
+          small: "(max-width: 499px)",
+          medium: "(min-width: 500px) and (max-width: 1199px)",
+          large: "(min-width: 1200px)"
+        }}>
+          {matches => (
+            <Fragment>
+              {matches.small && <>
+        <h2>Najbogatsze banki świata</h2>
+        <ul>
+          {data.map((bankInfo) => (<>
+            <li key={bankInfo.id}>{bankInfo.bank_name}</li> <br></br>  <br></br>
+            
+</>
+          ))}
+        </ul>
+       </>}
+              {matches.medium && <>
+        
+        <h2>Najbogatsze banki świata</h2>
+        <ul>
+          {data.map((bankInfo) => (<>
+            <li key={bankInfo.id}>{bankInfo.bank_name}</li>  <br></br>
+            </>
+          ))}
+        </ul>
+       </>}
+              {matches.large && <>
+        
+        <h2>Najbogatsze banki świata</h2>
+        <ul>
+          {data.map((bankInfo) => (
+            <li key={bankInfo.id}>{bankInfo.bank_name}</li>
+          ))}
+        </ul>
+       </>}
+            </Fragment>
+          )}
+        </Media> */}
+
+
+    </div>
+  );
+};
+export default Api;
---
+++ App.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CarbonatedDrinks from "../src/components/CarbonatedDrinks/Carbonated_drink";
+import Drinkers from "./components/Drinkers/Drinker";
+import Juices from "./components/Juices/Juice";
+import Nav from "./components/Nav/Nav";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Nav />,
+    children: [
+      {
+        path: "carbo",
+        element: <CarbonatedDrinks />
+      },
+      {
+        path: "drinkers",
+        element: <Drinkers />
+      },
+      {
+        path: "juices",
+        element: <Juices />
+      }
+    ]
+  }
+]);
+
+export default function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router}  />
+    </div>
+  );
+}
